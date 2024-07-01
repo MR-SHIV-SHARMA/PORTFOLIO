50c7b8953218c4fbe087b9ed5b6512cd4ae3ad64
mrshivsharma <shivps7568@gmail.com>
2024-07-01 18:15:41 +0530

    #

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-theta-taupe.vercel.app",
+        target: "http://localhost:8000",
       },
     },
   },
