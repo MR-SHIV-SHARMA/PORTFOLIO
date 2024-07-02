c2d21028e93c4629c791b7d9dc417434fa1e3222
mrshivsharma <shivps7568@gmail.com>
2024-07-02 10:24:31 +0530

    #

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://shivs-io.vercel.app",
+      '/api': {
+        target: 'https://shivs-io.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
+  build: {
+    outDir: 'dist',  // Ensure this matches your deployment settings
+  },
+  base: './', // Adjust the base path if needed
   plugins: [react()],
 });
