54ad4dcf2665bbdc04457eef24370f1764dda54b
mrshivsharma <shivps7568@gmail.com>
2024-07-04 08:36:15 +0530

    #

--- BACKEND/src/routes/contact.routes.js
+++ BACKEND/src/routes/contact.routes.js
@@ -17,5 +17,8 @@ import { contact } from "../controllers/contact.controllers.js";
 const router = Router();
 
 router.route("/contact").post(contact);
+router.get('/contact', (req, res) => {
+    res.json({ message: 'Welcome to the contect Page!' });
+});
 
 export default router;
