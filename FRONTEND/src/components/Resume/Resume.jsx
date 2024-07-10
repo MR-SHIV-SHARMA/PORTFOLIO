0ad31068f13a5b06ac5c085caf54aab1cab023e6
mrshivsharma <shivps7568@gmail.com>
2024-07-10 11:22:08 +0530

    #

--- FRONTEND/src/components/Resume/Resume.jsx
+++ FRONTEND/src/components/Resume/Resume.jsx
@@ -20,106 +20,123 @@ function Resume() {
                 <FiDownload size={24} />
               </a>
             </div>
-            <p>65 Khejri Bujurg</p>
-            <p>Jaipur, Rajasthan, 303901</p>
-            <p>6377930902</p>
-            <p>shivps7568@gmail.com</p>
-            <p>
-              <a
-                href="https://www.linkedin.com/in/shiv-prasad-sharma-490b732a1/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                LinkedIn
-              </a>
-            </p>
-            <p>
-              <a
-                href="https://shiv-io.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                Portfolio
-              </a>
-            </p>
-            <p>
-              <a
-                href="https://shiv-vibes.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                Project
-              </a>
-            </p>
+            <div className="flex  gap-2">
+              <p>+91 6377930902 </p>
+              <p>|</p>
+              <p>
+                <a
+                  href="https://github.com/MR-SHIV-SHARMA"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  Github
+                </a>
+              </p>
+              <p>|</p>
+              <p>
+                <a
+                  href="https://www.linkedin.com/in/shiv-prasad-sharma-490b732a1/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>|</p>
+              <p>
+                <a
+                  href="https://shiv-vibes.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  Project
+                </a>
+              </p>
+              <p>|</p>
+              <p>
+                <a
+                  href="https://x.com/Shiv_Sharma_786"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  Twitter
+                </a>
+              </p>
+            </div>
+            <p className="">shivps7568@gmail.com</p>
 
             <h2 className="text-lg font-semibold mt-4">PROFESSIONAL SUMMARY</h2>
-            <p>
-              Self-motivated BCA student and dedicated self-learner with strong
-              proficiency in JavaScript, HTML, CSS, and modern frameworks like
-              Tailwind CSS, ReactJS, and NextJS. Experienced in backend
-              development using NodeJS and database management with MongoDB.
-              Adept at creating responsive and dynamic web applications with a
-              keen eye for detail and a passion for coding and problem-solving.
+            <p className="mt-2">
+              Full stack developer with frontend and backend expertise, equipped
+              with a strong foundation and eager to contribute as a fresher.
             </p>
-
-            <h2 className="text-lg font-semibold mt-4">SKILLS</h2>
-            <ul className="list-disc list-inside">
+            <h2 className="text-lg font-semibold mt-4">TECHNICAL SKILLS</h2>
+            <ul className="list-disc list-inside mt-2">
               <li>
                 Programming Languages: JavaScript, basic knowledge of TypeScript
               </li>
               <li>
-                Frontend Technologies: HTML, CSS, Tailwind CSS, ReactJS, NextJS
+                Frontend: JavaScript, ReactJS, Redux, NextJS, basic knowledge of
+                TypeScript
               </li>
-              <li>Backend Technologies: NodeJS, ExpressJS</li>
+              <li>Backend: NodeJS, ExpressJS</li>
               <li>Database: MongoDB</li>
             </ul>
-
-            <h2 className="text-lg font-semibold mt-4">
-              PROFESSIONAL EXPERIENCE
-            </h2>
-            <h3 className="text-md font-medium mt-2">
-              Full-Stack Web Developer (Freelance)
-            </h3>
-            <p className="italic">SELF-EMPLOYED/PERSONAL PROJECTS</p>
-            <p>Jaipur (Remote) | January 2023 - Current</p>
-            <ul className="list-disc list-inside">
-              <li>
-                Developed a portfolio website utilizing ReactJS, MongoDB, and
-                JavaScript, showcasing a strong grasp of frontend and backend
-                technologies.
-              </li>
-              <li>
-                Created a comprehensive music academy website using HTML, CSS,
-                Tailwind CSS, ReactJS, NextJS, NodeJS, MongoDB, and TypeScript,
-                demonstrating versatility in modern web development frameworks.
-              </li>
-              <li>
-                Currently engaged in developing a basic e-commerce website,
-                further enhancing skills in creating dynamic and user-friendly
-                web applications.
-              </li>
-            </ul>
-
+            <h2 className="text-lg font-semibold mt-4">PROJECTS</h2>
+            <p className="mt-2">
+              <strong>
+                <a
+                  href="https://shiv-vibes.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  Music Academy Website
+                </a>
+                - Frontend and Backend Development
+              </strong>
+              <br />
+              <span className="italic font-bold">Tech Stack:</span> HTML, CSS,
+              Tailwind CSS, ReactJS, NextJS, NodeJS, MongoDB, TypeScript
+              <br />
+              <span className="italic font-bold">Frontend:</span> Next.js and
+              React.js for server-side rendering and logic, TypeScript for type
+              checking, Tailwind CSS for responsive design, and React Hooks for
+              state management.
+              <br />
+              <span className="italic font-bold">Backend:</span> Next.js manages
+              backend logic and API routes, Axios ensures smooth
+              frontend-backend communication for HTTP requests.
+              <br />
+              <span className="italic font-bold">Database:</span> MongoDB with
+              Mongoose for structured schema and efficient data handling.
+              <br />
+              <span className="italic font-bold">Authentication:</span> JWT for
+              secure user sessions and permissions, integrated with Next.js for
+              authentication and authorization.
+              <br />
+              <span className="italic font-bold">Features:</span> React Toastify
+              for customizable notifications, enhancing user interaction with
+              real-time feedback and alerts.
+            </p>
             <h2 className="text-lg font-semibold mt-4">EDUCATION</h2>
-            <p>
-              Bachelor of Computer Applications (BCA) in Computer Science and
-              Programming
+            <p className="mt-2">
+              University of Technology, Vatika | Bachelor of Computer
+              Applications | Expected Graduation: June 2025
             </p>
-            <p>
-              University of Technology, Vatika | Expected Graduation: June 2025
+            <h2 className="text-lg font-semibold mt-4">
+              ADDITIONAL INFORMATION
+            </h2>
+            <p className="mt-2">
+              Actively engage in personal projects and coding challenges to
+              expand technical proficiency and problem-solving skills.
+              Enthusiastic learner with a strong interest in exploring emerging
+              technologies and their applications in software development.
             </p>
-            <p>
-              Area of Study: Web Development, Database Management Systems,
-              Software Engineering, Data Structures, and Algorithms
-            </p>
-            <p>
-              School: Deepshikha Group of Colleges (Regional College),
-              University of Technology
-            </p>
-            <p>Percentage/CGPA/GPA: 60 to 70%</p>
           </div>
         </div>
       </div>
