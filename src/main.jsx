5e6c63e0c95753ea727c25732787d362069487d3
MR-SHIV-SHARMA <shivps7568@gmail.com>
2025-03-20 12:33:16 +0530

    refactor: add Preloader component to enhance loading experience in App

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./Layout.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Preloader from "./Preloader.jsx"; // Import Preloader
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 
 const router = createBrowserRouter([
@@ -19,7 +21,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [showPreloader, setShowPreloader] = useState(true);
+
+  return showPreloader ? (
+    <Preloader onFinish={() => setShowPreloader(false)} />
+  ) : (
+    <RouterProvider router={router} />
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
